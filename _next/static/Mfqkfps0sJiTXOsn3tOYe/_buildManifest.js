26657594d460a4be45d6c07c96df5b69510d0bbb
SyMind <dacongsama@live.com>
2022-09-18 13:01:31 +0000

    Deploying to gh-pages from @ SyMind/ji@43c0dc51c5effa050251b0881bfa7ac2564dd853 🚀

--- _next/static/Mfqkfps0sJiTXOsn3tOYe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e365c5169f423f16.css","static/chunks/pages/index-95c756286533d72b.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
