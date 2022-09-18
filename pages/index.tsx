ab6d1d0a4f803e55c2340216eed6e6b30738d0cb
SyMind <dacongsama@live.com>
2022-09-18 11:29:02 +0800

    use nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      🐔只因你太美
+    </div>
+  )
+}
+
+export default Home
