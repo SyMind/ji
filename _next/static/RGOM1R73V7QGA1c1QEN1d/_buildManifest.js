a385fb6a35a8db9e0433dee0b3e0a7b46a0af2af
SyMind <dacongsama@live.com>
2022-09-20 15:44:50 +0000

    Deploying to gh-pages from @ SyMind/ji@bc3b16e8a7f5fe7313b94766cb07c6f35945cfe8 🚀

---
+++ _next/static/RGOM1R73V7QGA1c1QEN1d/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/957d7fef86446e7e.css","static/chunks/pages/index-eefefd34862c9708.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/dbKgDxyIcA66cD_-OYAXP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/957d7fef86446e7e.css","static/chunks/pages/index-f9728e5378b9340b.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
